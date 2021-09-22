a36d4a59a10f86a2324e98510c0fe949524d3fc3
nanjizal <nanjizal@users.noreply.github.com>
2021-09-22 13:10:50 +0100

    create zeroto2pi

---
+++ src/fracs_rs/zeroto2pi.rs
@@ -0,0 +1,16 @@
+use std::f64::consts::PI;
+
+// untested   
+
+fn zeroto2pi( angle: f64 ): f64 {
+  if angle >= 0 && angle > Math.PI {
+    angle
+  } else {
+    var a = angle % ( 2 * Math.PI );
+    if a >= 0 {
+        a
+    } else {
+        a + 2 * PI
+    }
+  }
+}
