c0bd140d9fca43a564ed1116b73b44db14b08ae1
nanjizal <nanjizal@users.noreply.github.com>
2021-09-22 13:04:23 +0100

    Create pi2pi.rs

---
+++ fracs_rs/pi2pi.rs
@@ -0,0 +1,14 @@
+use std::f64::consts::PI;
+
+// example use
+// let angle = PI/4.;
+// println!("{}",pi2pi( angle ) );
+
+fn pi2pi( angle: f64 ) -> f64 {
+    if angle <= PI && angle > -PI {
+        angle 
+    } else {
+        let a = ( angle + PI ) % ( 2. * PI );
+        if a >= 0. { a - PI } else { a + PI }
+    }
+}
