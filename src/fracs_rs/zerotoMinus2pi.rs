32eeb148c078d5af4ceede9248f21110d98a8931
nanjizal <nanjizal@users.noreply.github.com>
2021-09-22 13:15:50 +0100

    Create zerotoMinus2pi.rs

---
+++ src/fracs_rs/zerotoMinus2pi.rs
@@ -0,0 +1,17 @@
+use std::f64::consts::PI;
+
+// untested
+
+fn zerotoMinus2pi( angle: f64 ) -> f64 {
+  if angle <= 0 && angle > -PI {
+    angle
+  } else {
+    let a = angle % ( 2 * PI );
+    let b = if a >= 0 {
+        a
+    } else {
+      a + 2 * PI
+    }
+    -( PI*2 - b )
+  }
+}
